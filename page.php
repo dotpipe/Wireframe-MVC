58f41a503f1ed68d8d466d3d1e809f07270d8b74
swatchphp <tcp@null.net>
2019-01-18 14:48:07 -0500

    Update page.php

--- page.php
+++ page.php
@@ -1,8 +1,4 @@
 <?php
-namespace Pirodock\Container;
-
-include('required.php');
-
 	class PageControllers {
 
         public $token;
@@ -418,27 +414,4 @@ include('required.php');
             return 1;
         }
     }
-    $y = array("Address" => "BenSt", "Duration" => "fixed");
-    $z = array("Address" => "25th", "Duration" => "limited");
-
-    $x = new PageControllers("adp");
-    $x->newView("BestPHPEverNow");
-    $x->mvc['index']->addModelField("Address");
-    $x->mvc['index']->addModelValid("Address",'/.*/');
-    $x->mvc['index']->addModelValid("Duration",'/.*/');
-    $x->mvc['index']->addModelData('index', $y);
-    
-    $x->mvc['BestPHPEverNow']->view->addPartial("index.php");
-    $x->mvc['BestPHPEverNow']->addModelValid("Address",'/1.*/');
-    $x->mvc['BestPHPEverNow']->addModelValid("Duration",'/.*/');
-    $x->mvc['BestPHPEverNow']->addModelData('index', $y);
-    $x->mvc['BestPHPEverNow']->addModelData('friends', $z);
-    $x->mvc['BestPHPEverNow']->view->writePage("BestPHPEverNow");
-    $x->save();
-    $x->paginateModels('BestPHPEverNow',0,2);
-    echo json_encode($x);
-    $x = $x->loadJSON();
-
-    echo "<br><br><br>";
-    echo json_encode($x);
     
