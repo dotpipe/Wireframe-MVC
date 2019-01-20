392992ac716a46d78127603489231091ee9c2c81
swatchphp <tcp@null.net>
2019-01-20 09:15:04 -0500

    Update test.php

--- test.php
+++ test.php
@@ -11,15 +11,21 @@
     $x->mvc['index']->addModelData('index', $y);
     
     $x->mvc['BestPHPEverNow']->view->addPartial("index.php");
-    $x->mvc['BestPHPEverNow']->addModelValid("Address",'/1.*/');
+    $x->mvc['BestPHPEverNow']->view->addPartial("index.php");
+    $x->mvc['BestPHPEverNow']->view->addShared("index.php");
+    $x->mvc['BestPHPEverNow']->addModelValid("Address",'/1.*/', "Please ask the admin to help you!");
     $x->mvc['BestPHPEverNow']->addModelValid("Duration",'/.*/');
     $x->mvc['BestPHPEverNow']->addModelData('index', $y);
     $x->mvc['BestPHPEverNow']->addModelData('friends', $z);
+    $x->paginateModels('BestPHPEverNow', 'index.php',0,2);
+    $x->mvc['BestPHPEverNow']->view->removeDependency("shared","index.php");
     $x->mvc['BestPHPEverNow']->view->writePage("BestPHPEverNow");
     $x->save();
-    $x->paginateModels('BestPHPEverNow',0,2);
     echo json_encode($x);
     $x = $x->loadJSON();
 
+    $x->newView("ForeverNew");
+    
     echo "<br><br><br>";
     echo json_encode($x);
+    
