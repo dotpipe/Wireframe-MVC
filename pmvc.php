9872ea9f43bfa87d77b28cc355215a02a51ae66f
swatchphp <tcp@null.net>
2019-01-20 10:58:14 -0500

    Update pmvc.php

--- pmvc.php
+++ pmvc.php
@@ -567,32 +567,3 @@ include('required.php');
             return 1;
         }
     }
-    $y = array("Address" => "BenSt", "Duration" => "fixed");
-    $z = array("Address" => "25th", "Duration" => "limited");
-
-    $x = new PageControllers("adp");
-    $x->newView("BestPHPEverNow");
-    $x->mvc['index']->addModelField("Address");
-    $x->mvc['index']->addModelValid("Address",'/.*/');
-    $x->mvc['index']->addModelValid("Duration",'/.*/');
-    $x->mvc['index']->addModelData('index', $y);
-    
-    $x->mvc['index']->view->addPartial("index.php");
-    $x->mvc['BestPHPEverNow']->view->addPartial("index.php");
-    $x->mvc['BestPHPEverNow']->view->addShared("index.php");
-    $x->mvc['BestPHPEverNow']->addModelValid("Address",'/1.*/', "Please ask the admin to help you!");
-    $x->mvc['BestPHPEverNow']->addModelValid("Duration",'/.*/');
-    $x->mvc['BestPHPEverNow']->addModelData('index', $y);
-    $x->mvc['BestPHPEverNow']->addModelData('friends', $z);
-    $x->paginateModels('index', 'index.php',1,3);
-    $x->mvc['BestPHPEverNow']->view->removeDependency("shared","index.php");
-    $x->mvc['index']->view->writePage("index");
-    $x->save();
-    echo json_encode($x);
-    $x = $x->loadJSON();
-
-    $x->addAction("adp","index");
-    
-    echo "<br><br><br>";
-    echo json_encode($x);
-    
