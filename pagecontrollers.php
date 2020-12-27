a611d35777303ce6d426d6a14fe7a9d65342444c
wise-penny <inland14@live.com>
2020-12-27 15:03:00 -0500

    Add files via upload

--- pagecontrollers.php
+++ pagecontrollers.php
@@ -1,57 +1,58 @@
 <?php
-spl_autoload_register(function ($className) {
-    foreach ([
-        '/src/lib/'
-    ] as $Path) {
-        if (!file_exists($Path . $className . '.php')) {
-            continue;
-        }
-        include $Path . $className . '.php';
-    }
-});
+namespace wise\src\wireframe;
 
-class PageControllers {
+require_once __DIR__ . '../../../../vendor/autoload.php';
 
+class PageControllers
+{
     public $token;
     public $view;
     public $mvc = array();
     public $md;
     public $sid = array();
     /**
-     * public function __construct
-     * @parameters string, string
+     * @method __construct
+     * @param string, string
      *
      */
-    function __construct(string $tok, string $view = 'index') {
+    public function __construct(string $tok, string $view = 'index')
+    {
         $this->mvc = null;
         $this->token = $tok;
-        if (!is_dir("$this->token"))
+        if (!is_dir("$this->token")) {
             mkdir("$this->token");
+        }
         if (!is_dir("$this->token")) {
             echo "Unable to create directories needed";
         }
-        if (!is_dir("$this->token/view/"))
+        if (!is_dir("$this->token/view/")) {
             mkdir("$this->token/view");
+        }
         if (!is_dir("$this->token/view")) {
             echo "Unable to create directories needed";
         }
-        if (!is_dir($this->token."/view/".$_COOKIE['PHPSESSID']))
+        if (!is_dir($this->token."/view/".$_COOKIE['PHPSESSID'])) {
             mkdir($this->token."/view/".$_COOKIE['PHPSESSID']);
+        }
         if (!is_dir($this->token."/view/".$_COOKIE['PHPSESSID'])) {
             echo "You do not have Cookies Enabled";
             exit();
         }
-        if (!is_dir("$this->token/view/shared"))
+        if (!is_dir("$this->token/view/shared")) {
             mkdir("$this->token/view/shared");
+        }
         if (!is_dir("$this->token/view/shared")) {
             echo "Unable to create directories needed";
         }
-        if (!file_exists("$this->token/config.json"))
+        if (!file_exists("$this->token/config.json")) {
             touch("$this->token/config.json");
-        if (!file_exists("$this->token/index.php"))
+        }
+        if (!file_exists("$this->token/index.php")) {
             touch("$this->token/index.php");
-        if (!file_exists("$this->token/view/shared/index.php"))
+        }
+        if (!file_exists("$this->token/view/shared/index.php")) {
             touch("$this->token/view/shared/index.php");
+        }
         if (!file_exists("$this->token/config.json")) {
             echo "Unable to create files needed";
         }
@@ -59,99 +60,103 @@ class PageControllers {
             echo "Unable to create files needed";
         }
         if (!file_exists("$this->token/view/shared/index.php")) {
-            echo "Unable to create files needed1";
+            echo "Unable to create files needed";
         }
         $this->path = "$this->token/view";
         $this->view = $view;
         $this->mvc[$tok] = new PageModels($view);
         $this->mvc[$tok]->view = new PageViews($tok, $view);
         $this->mvc[$tok]->md = $_COOKIE['PHPSESSID'];
-        setcookie('PHPSESSID', $_COOKIE['PHPSESSID'], time() * 60 * 60 * 24 * 365, "$this->token/$this->md");
+        setcookie('PHPSESSID', $_COOKIE['PHPSESSID'], time() + (60 * 60 * 24 * 365), "$this->token/$this->md");
         $this->mvc[$tok]->sid['model'] = new PageModels($_COOKIE['PHPSESSID']);
         $this->mvc[$tok]->sid['view'] = new PageViews($tok, $_COOKIE['PHPSESSID']);
     }
 
     /**
-     * public function addModelData
-     * @parameters string, array
+     * @method addModelData
+     * @param string, array
      *
      */
-    public function addModelData(string $view_name, array $data) {
+    public function addModelData(string $view_name, array $data): bool
+    {
         $this->mvc[$view_name]->addModelData($view_name, $data);
-        return 1;
+        return true;
     }
 
     /**
-     * public function save
-     * @parameters none
+     * @method save
+     * @param none
      *
      */
-    public function save() {
+    public function save(): bool
+    {
         $fp = fopen("$this->token/$this->md/config.json", "w");
         fwrite($fp, serialize($this));
         fclose($fp);
-        return 1;
+        return true;
     }
-
-
+    
+    
     /**
-     * public function paginateModels
-     * @parameters string, string, int, int
+     * @method paginateModels
+     * @param string, string, int, int
      *
      */
-    public function paginateModels(string $view_name, string $filename, int $begin = 0, int $end = 0) {
+    public function paginateModels(string $view_name, string $filename, int $begin = 0, int $end = 0)
+    {
         $x = $this->mvc[$this->token]->paginateModels($this->token, $view_name, $filename, $begin, $end);
         return $x;
-
     }
-
+    
     /**
-     * private function add_view
-     * @parameters string
+     * @method add_view
+     * @param string
      *
      */
-    private function add_view(string $view_name) {
-        if (is_dir("/$this->path/$view_name/")) {
-            if (!file_exists("/$this->path/$view_name/index.php")) {
-                $fp = fopen("/$this->path/$view_name/index.php", "w");
+    public function add_view(string $view_name): bool
+    {
+        if (is_dir("$this->path/$view_name/")) {
+            if (!file_exists("$this->path/$view_name/index.php")) {
+                $fp = fopen("$this->path/$view_name/index.php", "w");
                 fclose($fp);
             }
-
-        }
-        else {
-            mkdir("/$this->path/$view_name");
-            if (!is_dir("/$this->path/$view_name")) {
+        } else {
+            mkdir("$this->path/$view_name");
+            if (!is_dir("$this->path/$view_name")) {
                 echo "Permissions Error: Unable to create Directory";
-                return 0;
+                return false;
             }
-
-            touch("/$this->path/$view_name/index.php");
+            touch("$this->path/$view_name/index.php");
             touch("$this->token/config.json");
         }
         $this->mvc[$view_name] = new PageModels($view_name);
         $this->mvc[$view_name]->view = new PageViews($this->token, $view_name);
-        return 1;
+        return true;
     }
-
+    
     /**
-     * public function newView
-     * @parameters string
+     * @method newView
+     * @param string
      *
      */
-    public function newView(string $view_name) {
+    public function newView(string $view_name): bool
+    {
         $this->add_view($view_name);
+        return true;
     }
-
+    
     /**
-     * public function loadJSON
-     * @parameters none
+     * @method loadJSON
+     * @param none
      *
      */
-    public function loadJSON() {
-        if (file_exists("$this->token/config.json") && filesize("$this->token/config.json") > 0)
+    public function loadJSON()
+    {
+        if (file_exists("$this->token/config.json") && filesize("$this->token/config.json") > 0) {
             $fp = fopen("$this->token/config.json", "r");
-        else
-            return 0;
+        } else {
+            return false;
+        }
         $json_context = fread($fp, filesize("$this->token/config.json"));
         $old = unserialize($json_context);
         $b = $old->mvc[$old->token];
@@ -160,31 +165,24 @@ class PageControllers {
         }
         return $old;
     }
-
+    
     /**
-     * public function addPartial
-     * @parameters string
+     * @method addPartial
+     * @param string
      *
      */
-    public function addPartial(string $filename) {
-        return $this->view->addPartial($filename);
+    public function addPartial(string $filename)
+    {
+        return $this->mvc[$this->token]['view']->addPartial($filename);
     }
-
+    
     /**
-     * public function addShared
-     * @parameters string
+     * @method addShared
+     * @param string
      *
      */
-    public function addShared(string $filename) {
-        return $this->view->addShared($filename);
-    }
-
-    /**
-     * public function addAction
-     * @parameters string, string, string
-     *
-     */
-    public function addAction(string $token, string $view_name, string $action_name) {
-        return $this->actions[] = new PageActions($this->token, $view_name, $action_name);
+    public function addShared(string $filename)
+    {
+        return $this->mvc[$this->token]['shared']->addPartial($filename);
     }
 }
--- pageviews.php
+++ pageviews.php
@@ -1,156 +1,162 @@
 <?php
-spl_autoload_register(function ($className) {
-    foreach ([
-        '/src/lib/',
-        './'
-    ] as $Path) {
-        if (!file_exists($Path . $className . '.php')) {
-            continue;
-        }
-        include $Path . $className . '.php';
-    }
-});
 
-class PageViews {
+namespace wise\src\wireframe;
 
+require_once __DIR__ . '../../../../vendor/autoload.php';
+
+class PageViews
+{
     public $path;
     public $partials = array();
     public $token;
     public $injections = array();
     public $selector;
-
+    public $md;
     /**
-     * public function __construct
-     * @parameters string, string, string
+     * @method __construct
+     * @param string, string
      *
      */
-    function __construct(string $token, string $view_name) {
+    public function __construct(string $token, string $view_name)
+    {
         $this->token = $token;
-        $this->path = "$this->token/view";
-
-        if (!is_dir("$this->path/$view_name") && !mkdir("$this->path/$view_name"))
+        $this->path = "$this->token\/view";
+        $this->md = $_COOKIE['PHPSESSID'];
+        if (!is_dir("$this->path/$view_name") && !mkdir("$this->path/$view_name")) {
             echo "Unable to create needed directories";
+        }
         $this->copy = $view_name;
         $this->injections = [];
     }
-
+    
     /**
-     * public function addPartial
-     * @parameters string, string, string
+     * @method addPartial
+     * @param string, string, string
      *
      */
-    public function addPartial(string $filename, string $view_name = "FALSE", string $res_dir = "FALSE") {
-        if ($view_name == "FALSE")
+    public function addPartial(string $filename, string $view_name = "FALSE", string $res_dir = "FALSE"): bool
+    {
+        if ($view_name == "FALSE") {
             $view_name = $this->copy;
-        if ($res_dir == "FALSE")
+        }
+        if ($res_dir == "FALSE") {
             $res_dir = "partials";
+        }
         $bool = 0;
-
+        
         $exp_dir = explode('/', $res_dir);
         $kmd = "";
         foreach ($exp_dir as $k) {
             $kmd .= $k . '/';
-            if (!is_dir("$this->path/$view_name/$kmd") && !mkdir("$this->path/$view_name/$kmd"))
+            if (!is_dir("$this->path/$view_name/$kmd") && !mkdir("$this->path/$view_name/$kmd")) {
                 echo "Unable to create directories";
+            }
         }
         if (!is_dir("$this->path/$view_name/$res_dir") && !mkdir("$this->path/$view_name/$res_dir")) {
             echo "Unable to create directories needed";
-            return 0;
+            return false;
         }
         if (!file_exists("$this->path/$view_name/$res_dir/$filename") && !touch("$this->path/$view_name/$res_dir/$filename")) {
             echo "Unable to create files needed";
-            return 0;
+            return false;
         }
         foreach ($this->injections as $k=>$v) {
-            if ($v[0] == $res_dir && $v[1] == $filename)
+            if ($v[0] == $res_dir && $v[1] == $filename) {
                 $bool = 1;
+            }
         }
-        if ($bool == 1)
-            return 0;
-        else
-            $this->injections[] = $res_dir . "/" . $filename;
-        return 1;
+        if ($bool == 1) {
+            return false;
+        } else {
+            array_push($this->injections, $res_dir . "/" . $filename);
+        }
+        return true;
     }
-
+    
     /**
-     * public function addShared
-     * @parameters string
+     * @method addShared
+     * @param string
      *
      */
-    public function addShared(string $filename) {
+    public function addShared(string $filename): bool
+    {
         $bool = 0;
-        if (!is_dir("$this->path/shared"))
+        if (!is_dir("$this->path/shared")) {
             mkdir("$this->path/shared");
+        }
         if (!is_dir("$this->path/shared")) {
             echo "Unable to create directories needed";
-            return 0;
+            return false;
         }
         if (!file_exists("$this->path/shared/$filename")) {
             touch("$this->path/shared/$filename");
             if (!file_exists("$this->path/shared/$filename")) {
                 echo "Unable to create files needed";
-                return 0;
+                return false;
             }
         }
         foreach ($this->injections as $k=>$v) {
-            if ($v[0] == "shared" && $v[1] == $filename)
+            if ($v[0] == "shared" && $v[1] == $filename) {
                 $bool = 1;
+            }
         }
-        if ($bool == 1)
-            return 0;
-        else
-            $this->injections[] = array("shared","$filename");
-        return 1;
+        if ($bool == 1) {
+            return false;
+        } else {
+            array_push($this->injections, array("shared","$filename"));
+        }
+        return true;
     }
 
     /**
-     * public function save
-     * @parameters none
+     * @method save
+     * @param none
      *
      */
-    public function save() {
+    public function save(): bool
+    {
         $fp = fopen($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json", "w");
         fwrite($fp, serialize($this));
         fclose($fp);
-        return 1;
+        return true;
     }
-
+    
     /**
-     * public function loadJSON
-     * @parameters none
+     * @method loadJSON
+     * @param none
      *
      */
-    public function loadThisJSON() {
-        if (file_exists($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json") && filesize($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json") > 0)
+    public function loadThisJSON(): bool
+    {
+        if (file_exists($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json") && filesize($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json") > 0) {
             $fp = fopen($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json", "r");
-        else
-            return 0;
+        } else {
+            return false;
+        }
         $json_context = fread($fp, filesize($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json"));
         $old = unserialize($json_context);
         $b = $old->mvc[$old->token];
         foreach ($b as $key => $val) {
             $this->mvc[$this->token]->sid->$key = $b->sid->$key;
         }
-        return 1;
+        return true;
     }
 
     /**
-     * For user usage;
-     * public function writeIndex
-     * @parameters none
+     * @method writeIndex
+     * @param none
      *
      */
-    private function writeThisIndex() {
+    public function writeThisIndex():void
+    {
         $buff = "<?php";
         $fp = fopen($this->token."/view/".$_COOKIE['PHPSESSID']."/index.php", "w");
         foreach ($this->injections as $k) {
-            $vk = $k[0];
-            $vv = $k[1];
+            $vk = $k;
             if ($vk == "shared") {
-                $buff .= "\r\n\tinclude(\"../shared/$vv\");";
-            }
-            else {
-                $buff .= "\r\n\tinclude(\"../index/$vk/$vv\");";
+                $buff .= "\r\n\tinclude_once(\"../shared/$vk\");";
+            } else {
+                $buff .= "\r\n\tinclude_once(\"../index/$vk\");";
             }
         }
         $buff .= "?>\r\n";
@@ -159,150 +165,158 @@ class PageViews {
     }
 
     /**
-     * For domain usage;
-     * public function writeIndex
-     * @parameters none
+     * @method writeIndex
+     * @param none
      *
      */
-    private function writeIndex() {
+    public function writeIndex(): void
+    {
         $buff = "<?php";
         $fp = fopen("$this->token/index.php", "w");
         foreach ($this->injections as $k) {
-            $vk = $k[0];
-            $vv = $k[1];
-            if ($vk == "shared") {
-                $buff .= "\r\n\tinclude(\"view/shared/$vv\");";
-            }
-            else {
-                $buff .= "\r\n\tinclude(\"view/index/$vk/$vv\");";
+            $vk = $k;
+            $vv = '';
+            if (is_array($vk) && $vk[0] == "shared") {
+                $buff .= "\r\n\tinclude_once(__DIR__ . '/view/shared/$vk[1]');";
+            } else {
+                $buff .= "\r\n\tinclude_once(__DIR__ . \"/view/$this->copy/$vk\");";
             }
         }
-        $buff .= "?>\r\n";
+        $buff .= "\r\n?>\r\n";
         fwrite($fp, $buff);
         fclose($fp);
     }
 
     /**
-     * public function is_session_started
+     * @method is_session_started
      * @return bool
      */
-    function is_session_started() {
-        if ( php_sapi_name() !== 'cli' ) {
-            if ( version_compare(phpversion(), '5.4.0', '>=') ) {
-                return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+    public function is_session_started(): bool
+    {
+        if (php_sapi_name() !== 'cli') {
+            if (version_compare(phpversion(), '5.4.0', '>=')) {
+                return session_status() === PHP_SESSION_ACTIVE ? true : false;
             } else {
-                return session_id() === '' ? FALSE : TRUE;
+                return session_id() === '' ? false : true;
             }
         }
-            return FALSE;
+        return false;
     }
 
     /**
-     * For user usage;
-     * public function configJSON
-     * @parameters string
+     * @method configJSON
+     * @param string
      *
      */
-    public function configPageWrite(string $view_name = "index") {
+    public function configPageWrite(string $view_name = "index"): bool
+    {
         $fp = null;
 
-        if (!is_dir($this->token."/view/".$_COOKIE['PHPSESSID']) && !mkdir($this->token."/view/".$_COOKIE['PHPSESSID']))
+        if (!is_dir($this->token."/view/".$_COOKIE['PHPSESSID']) && !mkdir($this->token."/view/".$_COOKIE['PHPSESSID'])) {
             echo "Unable to create directories";
+        }
         if ($view_name == "index") {
             touch($this->token."/view/".$_COOKIE['PHPSESSID']."/index.php");
             $this->writeIndex($this->md);
-            return 1;
-        }
-        else if (!file_exists($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json"))
+            return true;
+        } elseif (!file_exists($this->token."/view/".$_COOKIE['PHPSESSID']."/config.json")) {
             echo "Unable to find files needed: ".$this->token."/view/".$_COOKIE['PHPSESSID']."/config.json";
-        else if ($view_name != "index")
+        } elseif ($view_name != "index") {
             $fp = fopen($this->token."/view/".$_COOKIE['PHPSESSID']."/index.php", "a");
+        }
         $dis = $this->loadThisJSON();
         $buff = "<?php\r\n";
         foreach ($this->injections as $k) {
-            $vk = $k[0];
-            $vv = $k[1];
+            $vk = $k;
             if ($vk == "shared") {
-                $buff .= "include(\"../shared/$vv\");\r\n";
+                $buff .= "include_once(\"../shared/$vk\");\r\n";
+            } elseif ($vk == "partials") {
+                $buff .= "include_once(\"../$view_name/$vk\");\r\n";
+            } else {
+                $buff .= "include_once(\"../view/".$_COOKIE['PHPSESSID']."/$vk\");\r\n";
             }
-            else if ($vk == "partials")
-                $buff .= "include(\"../$view_name/$vk/$vv\");\r\n";
-            else
-                $buff .= "include(\"../view/".$_COOKIE['PHPSESSID']."/$vk/$vv\");\r\n";
         }
         $buff .= "?>\r\n";
         fwrite($fp, $buff);
         fclose($fp);
-        return 1;
+        return true;
     }
 
 
     /**
-     * For domain usage;
-     * public function writePage
-     * @parameters string
+     * @method writePage
+     * @param string
      *
      */
-    public function writePage(string $view_name) {
+    public function writePage(string $view_name = "index"): bool
+    {
         $fp = null;
-        if ($view_name == "index") {
-            touch("$this->token/index.php");
-            $this->writeIndex();
-            return 1;
-        }
-        else if (!dir_exists("$this->token/$this->md") && !mkdir("$this->path/$this->md"))
-            echo "Unable to create directory needed";
-        else if (!file_exists("$this->token/$this->md/index.php") && !touch("$this->token/$this->md/index.php"))
-            echo "Unable to create files needed";
-        if ($view_name != "index")
-            $fp = fopen("$this->token/$this->md/index.php", "w");
-        $buff = "<?php\r\n";
-        foreach ($this->injections as $k) {
-            $vk = $k[0];
-            $vv = $k[1];
-            if ($vk == "shared") {
-                $buff .= "include(\"../shared/$vv\");\r\n";
+        try {
+            if ($view_name == "index") {
+                \touch("$this->token/index.php");
+                $this->writeIndex();
+                return true;
             }
-            else if ($vk == "partials")
-                $buff .= "include(\"../$view_name/$vk/$vv\");\r\n";
-            else
-                $buff .= "include(\"../$view_name/$vk/$vv\");\r\n";
+            if (!\is_dir("$this->token/view/$this->md") && !\mkdir("$this->token/view/$this->md")) {
+                echo "Unable to create directory needed";
+            }
+            if (!\file_exists("$this->token/view/$this->md/index.php") && !\file_put_contents("$this->token/view/$this->md/index.php", "")) {
+                echo "Unable to create files needed";
+            }
+            if ($view_name != "index") {
+                $fp = \fopen("$this->token/view/$this->md/index.php", "w");
+            }
+            $buff = "<?php\r\n";
+            foreach ($this->injections as $k) {
+                $vk = $k;
+                if ($vk == "shared") {
+                    $buff .= "include_once(\"../shared/$vk\");\r\n";
+                } elseif ($vk == "partials") {
+                    $buff .= "include_once(\"../$view_name/$vk\");\r\n";
+                } else {
+                    $buff .= "include_once(\"../$view_name/$vk\");\r\n";
+                }
+            }
+            \fwrite($fp, $buff);
+            \fclose($fp);
+        } catch (\Exception $e) {
+            return true;
         }
-        $buff .= "?>\r\n";
-        fwrite($fp, $buff);
-        fclose($fp);
-        return 1;
     }
-
+    
     /**
-     * public function removeDependency
-     * @parameters string, string
+     * @method removeDependency
+     * @param string, string
      *
      */
-    public function removeDependency(string $folder, string $partial) {
+    public function removeDependency(string $folder, string $partial): bool
+    {
         $bool = 0;
         $k = [];
         foreach ($this->injections as $v) {
-            if ($v != array($folder,$partial))
+            if ($v != array($folder,$partial)) {
                 $k = array_merge($k, array($v));
-            else $bool = 1;
+            } else {
+                $bool = 1;
+            }
         }
         if ($bool == 1) {
             $this->injections = $k;
-            return 1;
+            return true;
         }
-        return 0;
+        return false;
     }
 
     /**
-     * public function createAction
-     * @parameters string
+     * @method createAction
+     * @param string
      *
      */
-    public function createAction(string $action_name) {
+    public function createAction(string $action_name): bool
+    {
         $this->actions[$this->copy] = new PageViews($this->token, $this->copy);
         $this->actions[$this->copy]->addPartial("index.php", $this->copy, $action_name);
         echo "<br><br><br>" . json_encode($this->actions);
-        return 1;
+        return true;
     }
 }
